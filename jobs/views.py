ac53c2ec2474bcf8c702d174388cc2373c7e3de9
dvy19 <divyadixit.311204@gmail.com>
2026-04-09 10:52:59 +0530

    changes in views.oy

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-
-from httpx import request
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
