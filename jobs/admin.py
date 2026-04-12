04cee12aa561cfa40505205aa7a314328a07628c
dvy19 <divyadixit.311204@gmail.com>
2026-04-12 19:58:46 +0530

    change in admin

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from models import Blog, BlogLike, Job, Posts
+from .models import Blog, BlogLike, Job, Posts
 
 # Register your models here.
 admin.site.register(Blog)
