136a396186d62ad7792846644b185f81ab1a4e95
dvy19 <divyadixit.311204@gmail.com>
2026-04-14 10:31:32 +0530

    changes in post

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -45,7 +45,11 @@ class PostSerializer(serializers.ModelSerializer):
     company_name = serializers.SerializerMethodField()
 
     user = serializers.StringRelatedField(read_only=True)
-    comments = CommentSerializer(many=True, read_only=True)
+    comments = serializers.SerializerMethodField()
+
+    def get_comments(self, obj):
+        return []  # or limit to 1–2
+    
     likes_count = serializers.SerializerMethodField()
 
     class Meta:
@@ -60,10 +64,19 @@ class PostSerializer(serializers.ModelSerializer):
             'company_name',
             'comments',
             'likes_count'
-        ]
+        ],
+        read_only_fields = ['id', 'created_at', 'user', 'full_name', 'company_name', 'comments', 'likes_count']
 
     def get_full_name(self, obj):
-        return obj.user.full_name  # adjust if different field name
+    # Recruiter
+        if hasattr(obj.user, 'recruiterprofile'):
+            return obj.user.recruiterprofile.full_name
+
+        # Seeker
+        if hasattr(obj.user, 'seekerprofile'):
+            return obj.user.seekerprofile.full_name
+
+        return None
 
     def get_company_name(self, obj):
         if hasattr(obj.user, 'recruiterprofile'):
